b4c5ce506d4b9cff7211eaaf0e8538a9c6fe8af7
Haiwen Chen <haiwen.chen311@gmail.com>
2016-01-25 19:23:36 -0800

    add update, show, edit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+	def new
+		@article = Article.new
+	end
+
+	def create
+		# render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		if(@article.save)
+			flash[:notice] = "article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if(@article.update(article_params))
+			flash[:notice] = "article was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
+
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title, :description)
+	end
+end
